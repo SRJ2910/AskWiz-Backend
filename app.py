a81d5e0627174ad812107fea36d220273900e1f5
SRJ <Suraj@SRJ>
2023-04-09 19:14:06 +0530

    flask version upgraded and some rempved unused var

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-from flask import Flask, jsonify, request
-import pandas as pd
-import numpy as np
+from flask import Flask, request
 import joblib
-import traceback
-from flask_restful import reqparse
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
@@ -15,28 +11,23 @@ def predict():
 	lr = joblib.load("dopaNet.pkl")
 	if lr:
 		try:
-			json = request.get_json()	 
-			# print(json.get("context"))
-			# list = [json.get("context"), json.get("question")]
-			# # # model_columns = joblib.load("model_cols.pkl")
-			# # temp=list(json["context"].values())
-			# print(list)
+			json = request.get_json()
+			
 			pred = {
 				'context': json.get("context"),
 				'question': json.get("question")
 			}
-			# # vals=np.array(list)
+
 			prediction = lr.predict(pred)
-			# print("here:",prediction)        
-			# return jsonify({'prediction': str(prediction[0])})
+
 			res = {
 				'prediction': prediction['answer'],
 				'score': prediction['score']
 			}
+			
 			return (res)
 
-		except:        
-			# return jsonify({'trace': traceback.format_exc()})
+		except:
 			return ('no')
 	else:
 		return ('No model here to use')
